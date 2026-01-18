6a6a46016801fbd12afca7b9caac34e694ac02fd
SumantArewar <sumantarewar.123@gmail.com>
2026-01-18 23:56:39 +0530

    Added Binary Search Class

---
+++ Day10/BinarySearchDemo.java
@@ -0,0 +1,34 @@
+package Day10;
+
+import java.util.Scanner;
+public class BinarySearchDemo {
+    static int BinarySearch(int[] arr, int x){
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(arr[mid] == x){
+                return mid;
+            }
+            if(arr[mid] < x){
+                start = ++mid;
+            }else{
+                end = --mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Target : - ");
+        int target = sc.nextInt();
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        // Binary Search Algorithm
+        int targetIndex = BinarySearch(arr, target);
+        System.out.println(targetIndex);
+    }
+}
