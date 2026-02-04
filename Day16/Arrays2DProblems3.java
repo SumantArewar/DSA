27ce3ce3133361e4f7212cab8828cc482ddc5366
SumantArewar <sumantarewar.123@gmail.com>
2026-02-04 23:51:16 +0530

    Added Brute Force Approach for sum of rectangle

--- Day16/Arrays2DProblems3.java
+++ Day16/Arrays2DProblems3.java
@@ -3,10 +3,18 @@ package Day16;
 public class Arrays2DProblems3 {
 
     // Sum of Rectangle (Brute Force Approach)
-
+    static int findSum(int[][] arr, int l1, int r1, int l2, int r2){
+        int sum = 0;
+        for(int i = l1; i <= l2; i++){
+            for(int j = r1; j <= r2; j++){
+                sum += arr[i][j];
+            }
+        }
+        return sum;
+    }
     // Sum of Rectangle (Precalculating Horizontal Sum of each row)
 
-    // Sum of Rectangle (Prefix Sum over both Rows & Coloums)
+    // Sum of Rectangle (Prefix Sum over both Rows & Columns)
 
     public static void main(String[] args) {
         int[][] arr = {
@@ -16,5 +24,7 @@ public class Arrays2DProblems3 {
                 {16, 17, 18, 19, 20},
                 {21, 22, 23, 24, 25},
         };
+        int sum = findSum(arr, 0, 0, 4, 4);
+        System.out.println("Sum of the selected Rectangle is - " + sum);
     }
 }
