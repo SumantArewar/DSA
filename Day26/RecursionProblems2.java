245b8df0dfb60167670e4042b0e037d563990748
SumantArewar <sumantarewar.123@gmail.com>
2026-02-19 23:41:26 +0530

    Update Recursion Problems 2 Class

--- Day26/RecursionProblems2.java
+++ Day26/RecursionProblems2.java
@@ -1,7 +1,16 @@
 package Day26;
 
 public class RecursionProblems2 {
+    static int sumOfDigits(int n){
+        int ans = 0;
+        // base case
+        if(n <= 0){
+            return 0;
+        }
+        return n % 10 + sumOfDigits(n/10);
+    }
     public static void main(String[] args) {
-
+        int sum = sumOfDigits(12345);
+        System.out.println("Sum of digits is - " + sum);
     }
 }
