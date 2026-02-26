b17923c4544016144a52d6b65ac8fe0d65893546
SumantArewar <sumantarewar.123@gmail.com>
2026-02-26 23:33:38 +0530

    Updated RecursionProblem5 class

--- Day29/RecursionProblem5.java
+++ Day29/RecursionProblem5.java
@@ -1,8 +1,17 @@
 package Day29;
 
 public class RecursionProblem5 {
+    static int printMax(int[] arr, int i){
+        int max = Integer.MIN_VALUE;
+//        System.out.println(max);
+        if(arr[i] < arr[i-1]) max = arr[i-1];
+        if(i-1 == 0) return max;
+        printMax(arr, i-1);
+        return max;
+    }
     public static void main(String[] args) {
-
-        // Recursion on Arrays - code is yet to add
+        int[] arr = {1, 6, 3, 4, 5, 8};
+        int maxValue = printMax(arr, 4);
+        System.out.println(maxValue);
     }
 }
