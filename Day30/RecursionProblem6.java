f045b13d6a7fad58a39e5601ca7d7d51330dc20f
SumantArewar <sumantarewar.123@gmail.com>
2026-06-22 23:41:58 +0530

    Updated RecursionProblem 6 file

--- Day30/RecursionProblem6.java
+++ Day30/RecursionProblem6.java
@@ -1,11 +1,28 @@
 package Day30;
 
 public class RecursionProblem6 {
+
+    static boolean isExists (int[] arr, int target, int n, int idx){
+        if(idx >= n ) return false;
+        if(arr[idx] == target) return true;
+        return isExists(arr, target, n, idx+1);
+    }
+
+    static void findIndices (int[] arr, int target, int n, int idx){
+        if(idx >= n ) return;
+        if(arr[idx] == target) System.out.print(idx + " ");;
+        findIndices(arr, target, n, idx+1);
+    }
+
     public static void main(String[] args) {
-
-// Problems yet to add
-
-//..
+        int[] arr = {1, 6, 3, 4, 6, 5, 8, 6};
+        boolean ans = isExists(arr,6, arr.length, 0);
+        if(ans){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        findIndices(arr,6, arr.length, 0);
 
     }
 }
