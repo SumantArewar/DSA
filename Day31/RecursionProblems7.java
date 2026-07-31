6271913c4ff8cd5e00bfdd81809acfb0e463266f
SumantArewar <sumantarewar.123@gmail.com>
2026-08-01 04:49:27 +0530

    Updated RecursionProblem 7

--- Day31/RecursionProblems7.java
+++ Day31/RecursionProblems7.java
@@ -1,8 +1,19 @@
 package Day31;
 
 public class RecursionProblems7 {
+    static String removeOccurrences(String s, int idx){
+        if(idx == s.length()) return "";
+        String smallAns = removeOccurrences(s, idx+1);
+        char curr = s.charAt(idx);
+        if(curr != 'a'){
+            return curr + smallAns;
+        } else {
+            return smallAns;
+        }
+    }
     public static void main(String[] args) {
-
-
+        String s = "abacadafagahaja";
+        String Ans = removeOccurrences(s, 0);
+        System.out.println(Ans);
     }
 }
