4bcf1dcd94e1b7b977aa8f29fb9adbdfad07b4d8
SumantArewar <sumantarewar.123@gmail.com>
2026-08-03 23:59:47 +0530

    Updated recursion 7 with new method

--- Day31/RecursionProblems7.java
+++ Day31/RecursionProblems7.java
@@ -11,9 +11,17 @@ public class RecursionProblems7 {
             return smallAns;
         }
     }
+    static String reverseString(String s, int idx){
+        if(idx == s.length()) return "";
+        String smallAns = reverseString(s, idx+1);
+        return smallAns + s.charAt(idx);
+    }
     public static void main(String[] args) {
         String s = "abacadafagahaja";
+        String ss = "abcd";
         String Ans = removeOccurrences(s, 0);
-        System.out.println(Ans);
+        String Ans2 = reverseString(ss, 0);
+//        System.out.println(Ans);
+        System.out.println(Ans2);
     }
 }
