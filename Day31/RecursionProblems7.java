2eae8dbad85056a766a053e6b114a43463b62bed
SumantArewar <sumantarewar.123@gmail.com>
2026-08-05 23:59:55 +0530

    Add Recursion 8 file

--- Day31/RecursionProblems7.java
+++ Day31/RecursionProblems7.java
@@ -1,5 +1,7 @@
 package Day31;
 
+import java.util.Scanner;
+
 public class RecursionProblems7 {
     static String removeOccurrences(String s, int idx){
         if(idx == s.length()) return "";
@@ -11,17 +13,37 @@ public class RecursionProblems7 {
             return smallAns;
         }
     }
+
     static String reverseString(String s, int idx){
         if(idx == s.length()) return "";
         String smallAns = reverseString(s, idx+1);
         return smallAns + s.charAt(idx);
     }
+
+    static boolean isPalindrome(String s, int l, int r){
+        if(l >= r) return true;
+        return (s.charAt(l) == s.charAt(r) && isPalindrome(s, l + 1, r - 1));
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
         String s = "abacadafagahaja";
         String ss = "abcd";
         String Ans = removeOccurrences(s, 0);
-        String Ans2 = reverseString(ss, 0);
+        String revStr = reverseString(str, 0);
 //        System.out.println(Ans);
-        System.out.println(Ans2);
+//        System.out.println(revStr);
+//        if(str.equals(revStr)){
+//            System.out.printf("%s is Palindrome ", str);
+//        }else{
+//            System.out.printf("%s is not Palindrome ", str);
+//        }
+//        boolean isPalindrome = isPalindrome(str, 0, str.length()-1);
+//        if(isPalindrome){
+//            System.out.printf("%s is Palindrome ", str);
+//        }else{
+//            System.out.printf("%s is not Palindrome ", str);
+//        }
+        System.out.println(isPalindrome(str, 0, str.length()-1));
     }
 }
---
+++ Day32/RecursionProblems8.java
@@ -0,0 +1,5 @@
+package Day32;
+
+public class RecursionProblems8 {
+
+}
